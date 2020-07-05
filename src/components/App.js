6d707cf6543aa8a1b6d71e2d1c7a1609a1e5773c
senaru <s3696653@student.rmit.edu.au>
2020-07-05 23:52:50 +1000

    Change Structure and Import tut basic structure CSS

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+
+function App() {
+  return (
+    <main className="page bg-white" id="petratings">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 bg-white">
+            <div className="container">
+              <div>Add Appointments</div>
+              <div>Search Appointments</div>
+              <div>List Appointments</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
