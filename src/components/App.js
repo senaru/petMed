3ccfe1a9377d53717112e5f70a137be9759ed215
senaru <s3696653@student.rmit.edu.au>
2020-07-06 23:00:13 +1000

    Using fetch in a lifecycle method

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,50 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { Component } from 'react';
 import '../css/App.css';
 
 import AddAppointments from './AddAppointments';
 import ListAppointments from './ListAppointments';
 import SearchAppointments from './SearchAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <ListAppointments />
-              <SearchAppointments />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myName: "Senaru",
+      myAppointments: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(result => {
+        const apts = result.map(item => {
+          return item;
+        })
+        this.setState({
+          myAppointments: apts
+        });
+      });
+  }
+
+  render() {
+    return (
+      <main className="page bg-white" id="petratings" >
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                {this.state.myName}
+                <AddAppointments />
+                <ListAppointments />
+                <SearchAppointments />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  };
 }
 
 export default App;
