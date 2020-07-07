b42fee3703079cd32ebe8b7a24834662e75a73fb
senaru <s3696653@student.rmit.edu.au>
2020-07-07 13:00:53 +1000

    Monitor form state

--- src/components/AddAppointments.js
+++ src/components/AddAppointments.js
@@ -2,6 +2,29 @@ import React, { Component } from 'react';
 import { FaPlus } from 'react-icons/fa';
 
 class AddAppointments extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            petName: '',
+            ownerName: '',
+            aptDate: '',
+            aptTime: '',
+            aptNotes: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        const target = e.target;
+        const value = target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        })
+    }
+
     render() {
         return (
             <div
@@ -28,6 +51,8 @@ class AddAppointments extends Component {
                                     className="form-control"
                                     name="petName"
                                     placeholder="Pet's Name"
+                                    value={this.state.petName}
+                                    onChange={this.handleChange}
                                 />
                             </div>
                         </div>
@@ -45,6 +70,8 @@ class AddAppointments extends Component {
                                     className="form-control"
                                     name="ownerName"
                                     placeholder="Owner's Name"
+                                    value={this.state.ownerName}
+                                    onChange={this.handleChange}
                                 />
                             </div>
                         </div>
@@ -62,6 +89,8 @@ class AddAppointments extends Component {
                                     className="form-control"
                                     name="aptDate"
                                     id="aptDate"
+                                    value={this.state.aptDate}
+                                    onChange={this.handleChange}
                                 />
                             </div>
                             <label
@@ -76,6 +105,8 @@ class AddAppointments extends Component {
                                     className="form-control"
                                     name="aptTime"
                                     id="aptTime"
+                                    value={this.state.aptTime}
+                                    onChange={this.handleChange}
                                 />
                             </div>
                         </div>
@@ -92,6 +123,8 @@ class AddAppointments extends Component {
                                     name="aptNotes"
                                     id="aptNotes"
                                     placeholder="Appointment Notes"
+                                    value={this.state.aptNotes}
+                                    onChange={this.handleChange}
                                 />
                             </div>
                         </div>
